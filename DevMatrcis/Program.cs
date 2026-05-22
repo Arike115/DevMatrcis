dbf1c6e59b5d45f8915c316bf7b8d0e708a766c3
Arike115 <ajibolaoluwafunke669@gmail.com>
2026-05-22 14:04:51 +0100

    Add project files.

---
+++ DevMatrcis/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//operators
+//arithmetic operators +,-,*,/,++,--
+//logical //
+//relational // ==
+//assignment //
+
+int v = 50;
+int y = 78;
+
+int x = v + y;
+Console.WriteLine(x);
+
+
+int g = 5;
+int result = g++;
+Console.WriteLine(result);
+
+int l = 5;
+int value = l--;
+Console.WriteLine(value);
---
+++ DevMatrcis/Student.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace DevMatrcis
+{
+    internal class Student
+    {
+    }
+}
