28cb0935814215623a7b7cd676629db2bf8440de
Arike115 <ajibolaoluwafunke669@gmail.com>
2026-05-25 12:30:31 +0100

    assignmentoperator

--- DevMatrcis/Program.cs
+++ DevMatrcis/Program.cs
@@ -3,19 +3,9 @@
 //arithmetic operators +,-,*,/,++,--
 //logical //
 //relational // ==
-//assignment //
+//assignment // =, +=,-=,*=,/=
 
-int v = 50;
-int y = 78;
-
-int x = v + y;
-Console.WriteLine(x);
-
-
-int g = 5;
-int result = g++;
-Console.WriteLine(result);
-
-int l = 5;
-int value = l--;
-Console.WriteLine(value);
+int v = 10;
+v += 15;
+v -= 5;
+Console.WriteLine(v);
