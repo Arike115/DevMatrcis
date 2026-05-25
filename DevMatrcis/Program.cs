169b073f049a4ce8de007532b7640c224dbbb985
Arike115 <ajibolaoluwafunke669@gmail.com>
2026-05-25 13:36:54 +0100

    statement

--- DevMatrcis/Program.cs
+++ DevMatrcis/Program.cs
@@ -1,11 +1,78 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //operators
-//arithmetic operators +,-,*,/,++,--
-//logical //
-//relational // ==
-//assignment // =, +=,-=,*=,/=
+//logical // &&,||,!
+//relational // ==,!=,>,<,>=,=<
+//Statement
+//conditational statement
+//if else
+//switch case
+//loopings
+//foreach
+//for
+//do while
+//while
 
-int v = 10;
-v += 15;
-v -= 5;
-Console.WriteLine(v);
+
+int v = 7;
+int i = 5;
+int x = 5;
+int age = 18;
+
+//if else
+if (v > i)
+{
+    Console.WriteLine("welcome to you are in!");
+}
+else if (x == i)
+{
+    Console.WriteLine("not partially in let try again");
+}
+else
+{
+    Console.WriteLine("you are out!");
+}
+
+
+if (v < age || age == v || v > age)
+{
+    Console.WriteLine("welcome to you are in!");
+}
+else
+{
+    Console.WriteLine("you are out!");
+}
+
+
+//switch
+
+int day = 3;
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday Note: we don work on weekends");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+    default: 
+        Console.WriteLine("sorry invalid days");
+        break;
+}
+
+
+
