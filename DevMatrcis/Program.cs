bbb907040fe038fa16c02b703bebcac059c67522
Arike115 <ajibolaoluwafunke669@gmail.com>
2026-06-22 12:21:45 +0100

    generic examples

--- DevMatrcis/Program.cs
+++ DevMatrcis/Program.cs
@@ -1,7 +1,25 @@
-﻿
-using DevMatrcis;
+﻿//Generic is a powerful feature in C# that allows you to create classes, methods,
+//and data structures that can work with any data type. It provides type safety
+//and code reusability, enabling you to write more flexible and efficient code.
+//With generics, you can define a class or method with a placeholder for the type
+//it will operate on, which is specified when the class or method is instantiated
+//or called. This allows you to create collections, algorithms, and other constructs
+//that can work with different types without sacrificing performance or safety.
 
-ClassB inherit = new ClassB(); //instiation
-inherit.Register();
-var result = Gender.Female;
-Console.WriteLine(result);
+
+using DevMatrcis;
+using Microsoft.VisualBasic;
+string x = " ";
+
+Infomation<string>.ShowEachInfo("Agboola");
+Infomation<int>.ShowEachInfo(678);
+Infomation<string>.DisplayInfo("Hello", "World");
+
+
+
+string message = "this is the testament that God is good";
+int v = 789;
+var count = message.RegionCount();
+Console.WriteLine(count);
+
+var result2 = "0908756****";
---
+++ DevMatrcis/RegionSet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrcis
+{
+    //extension method is a special kind of static method, but they are called
+    //as if they were instance methods on the extended type.
+
+    //must be a static class to contain extension methods.
+    public static class RegionSet
+    {
+        
+        //must be static  method
+        //first parameter use 'this' keyword followed by the type
+        //that the method extends.
+        public static int RegionCount(this string input)
+        {
+            
+            if (string.IsNullOrWhiteSpace(input))
+                return 0;
+            
+            var result = input.Split(new char[] { ' ', '\t', '\n' }, 
+                StringSplitOptions.RemoveEmptyEntries);
+            return result.Length;
+
+        }
+    }
+}
