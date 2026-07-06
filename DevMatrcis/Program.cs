4d4c2ca28a0e685557148cf9e2423700d21f7bda
Arike115 <ajibolaoluwafunke669@gmail.com>
2026-07-06 12:18:57 +0100

    stack, list, icollection,ilist,ienumerable

--- DevMatrcis/Program.cs
+++ DevMatrcis/Program.cs
@@ -1,41 +1,26 @@
-﻿//Ilist
-//list
-//collection
-//ienumerable
+﻿//stack last in first out
+//queue
 
-List<string> names = new List<string> ();   
-names.Add("John");
-names.Add("Jane");
-names.Add("Bob");
-names.Add("Alice");
-names.Add("Charlie");
-names.Add("David");
-names.Add("Eve");
-MyIlistMethod(names);
-MycollectionMethod(names);
-
-foreach (var name in names)
-{
-    Console.WriteLine(name);
-}
-MyIEnumerableMethod(names);
-
-static void MyIlistMethod(IList<string> myList)
-{
-    myList.Add("Frank");
-    myList.Insert(2, "Grace");
-}
-
-static void MycollectionMethod(ICollection<string> myCollection)
-{
-    myCollection.Add("Hank");
-    myCollection.Remove("Alice");
-}   
+using System;
+Stack<int> stack = new Stack<int>();
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+stack.Push(4);
+stack.Push(5);
 
 
-static void MyIEnumerableMethod(IEnumerable<string> myEnumerable)
-{
-    var result = myEnumerable.Count();
-    Console.WriteLine(result);
-  
-}
+//stack.Pop();
+//Console.WriteLine(stack.Count());
+//stack.Pop();
+//Console.WriteLine(stack.Count());
+
+//while (stack.Count() > 0)
+//{
+//   Console.WriteLine("Total" + stack.Count());
+//    Console.WriteLine(stack.Pop());
+//}
+
+
+foreach (int i in stack)
+    Console.WriteLine(i);
