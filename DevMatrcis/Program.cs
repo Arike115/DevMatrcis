83aa331df3a8d8c303f7499ebd40bd266a5e3e0f
Arike115 <ajibolaoluwafunke669@gmail.com>
2026-07-06 13:13:03 +0100

    dictionary

--- DevMatrcis/Program.cs
+++ DevMatrcis/Program.cs
@@ -1,26 +1,46 @@
-﻿//stack last in first out
-//queue
+﻿//queue first in first out
+//Dictionary is a collection of key value pairs, which is used to store data in a
+//key value pair format. It is similar to a list,
+//but it allows you to access values using keys instead of indexes.
+// The Dictionary class is a generic collection,
+// which means that you can specify the type of keys and values that it will store.
+// The Dictionary class provides methods for adding,
+// removing, and accessing key value pairs,
+// as well as for checking if a key exists in the dictionary.
 
 using System;
-Stack<int> stack = new Stack<int>();
-stack.Push(1);
-stack.Push(2);
-stack.Push(3);
-stack.Push(4);
-stack.Push(5);
+
+Dictionary<int, string> dictionaryValues = new Dictionary<int, string>();
+dictionaryValues.Add(1,"C#");
+dictionaryValues.Add(2,"Java");
+dictionaryValues.Add(3,"Python");
+dictionaryValues.Add(4,"JavaScript");
+dictionaryValues.Add(5,"C++");  
+
+foreach (var item in dictionaryValues)
+{
+    Console.WriteLine("Chapter: " + item.Key + ", Value: " + item.Value);
+}
 
 
-//stack.Pop();
-//Console.WriteLine(stack.Count());
-//stack.Pop();
-//Console.WriteLine(stack.Count());
-
-//while (stack.Count() > 0)
-//{
-//   Console.WriteLine("Total" + stack.Count());
-//    Console.WriteLine(stack.Pop());
-//}
 
 
-foreach (int i in stack)
-    Console.WriteLine(i);
+
+
+
+
+
+//Queue<string> queue = new Queue<string>();    
+//queue.Enqueue("Toyota");
+//queue.Enqueue("Honda");
+//queue.Enqueue("Ford");
+//queue.Enqueue("Chevrolet");
+//queue.Enqueue("Nissan");
+
+//Console.WriteLine("the total count is " + queue.Count());
+//Console.WriteLine("the first element is " + queue.Dequeue()); 
+//Console.WriteLine("the final total count is " + queue.Count());
+//Console.WriteLine("the first element is " + queue.Dequeue());
+//Console.WriteLine("the final total count is " + queue.Count());
+
+
