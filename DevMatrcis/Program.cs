5a74cea94d2a02db26b59925ddbb334bacc860e9
Arike115 <ajibolaoluwafunke669@gmail.com>
2026-07-20 13:21:18 +0100

    programming examples

--- DevMatrcis/Program.cs
+++ DevMatrcis/Program.cs
@@ -1,66 +1,43 @@
-﻿//innerjoin join
-//leftjoin leftouterJoin
-//rightJoin rightouterjoin
-//fulljoin
+﻿using System.Runtime.ExceptionServices;
 
-using DevMatrcis;
-
-List<Employee> employee = new List<Employee>() 
-{ 
-    new Employee(){ Id = 1, Name = "John",Age = 36,Gender= Gender.Male, Department_id=1,},
-    new Employee(){ Id = 2, Name = "Jane",Age = 30,Gender= Gender.Female, Department_id=2,},
-    new Employee(){ Id = 3, Name = "Bob", Age = 33,Gender= Gender.Male,Department_id=1,},
-    new Employee(){Id = 4, Name = "Alice", Age = 40, Gender = Gender.Female, Department_id = 6},
-    new Employee(){Id = 5, Name = "Tom", Age = 31, Gender = Gender.Male, Department_id = 2},
-    new Employee(){Id = 6, Name = "Jerry", Age = 26, Gender = Gender.unknown, Department_id = 3},
-    new Employee(){Id = 7, Name = "Mike", Age = 22, Gender = Gender.Male, Department_id = 4},
-    new Employee(){Id = 8, Name = "Sara", Age = 29, Gender = Gender.Female, Department_id = 2},
-    new Employee(){Id = 9, Name = "David", Age = 25, Gender = Gender.Male, Department_id = 4},
-    new Employee(){Id = 10, Name = "Emma", Age = 32, Gender = Gender.Female},
-};
-
-
-List<Departments> departments = new List<Departments>()
+internal class Program
 {
-    new Departments(){ Id = 1, Name = "HR", Location = "New York"},
-    new Departments(){ Id = 2, Name = "IT", Location = "San Francisco"},
-    new Departments(){ Id = 3, Name = "Finance", Location = "Chicago"},
-    new Departments(){ Id = 4, Name = "Marketing", Location = "Los Angeles"},
-    new Departments(){ Id = 5, Name = "Sales", Location = "Houston"},
-};
+    private static void Main(string[] args)
+    {
+        LastProcess();
+        Firstprocess();
+        Console.ReadKey();
+    }
 
-//innerjoin
-//query syntax
-var innerJoin = from emp in employee
-                join dept in departments on emp.Department_id equals dept.Id
-                select new { emp.Name, emp.Gender,
-                  deptname =  dept.Name, dept.Location };
+    //synchronous method
+    static void Firstprocess()
+    {
+        Console.WriteLine("welcome to the first process world");
+        Console.WriteLine("First process starting now .........");
+        Console.WriteLine("First process completed");
+    }
 
-//method syntax
-var innerJoinMethod = employee.Join(departments, emp => emp.Department_id, dept => dept.Id,
-    (emp, dept) => new {
-        emp.Name, emp.Gender,
-        deptname = dept.Name,
-        dept.Location });
+    //asynchronous method
+    static async Task LastProcess()
+    {
+        Console.WriteLine("welcome to the last process world");
+        await Task.Delay(3000);
+        Console.WriteLine("last process starting now .........");
+        Console.WriteLine("last process completed");
+        await DataReturnProcess();
+    }
 
+    static async Task<bool> DataReturnProcess()
+    {
+        return true;
+    }
+    static bool DataProcess()
+    {
+        return true;
+    }
+    static async Task LongDataReturnProcess()
+    {
 
+    }
 
-//leftouter join
-
-//foreach (var item in innerJoin)
-//{
-//    Console.WriteLine($"Name: { item.Name}, Gender: { item.Gender},Department Name: { item.deptname}, Location: { item.Location}");
-// }
-
-//leftjoin 
-//query syntax
- var leftJoin = from emp in employee
-                join dept in departments on emp.Department_id equals dept.Id into empDept
-                from dept in empDept.DefaultIfEmpty()
-                select new { emp.Name, emp.Gender,
-                    deptname = dept != null ? dept.Name : "No Department",
-                    Location = dept != null ? dept.Location : "No Location" };
-foreach (var item in leftJoin)
-{
-    Console.WriteLine($"Name: {item.Name}, Gender: {item.Gender},Department Name: {item.deptname}, Location: {item.Location}");
 }
